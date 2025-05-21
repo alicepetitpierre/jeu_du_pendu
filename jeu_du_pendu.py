d18af02b83743ae32c11b05e22ac6c3e662c190e
alicepetitpierre <alice.petitpierre.1@ens.etsmtl.ca>
2025-05-21 13:33:06 -0400

    Fonctions séparées dans un module

--- jeu_du_pendu.py
+++ jeu_du_pendu.py
@@ -1,7 +1,29 @@
+#-----------------------------------------------
+#Nom du fichier : jeu_du_pendu.py
+#Description    : Jeu du pendu
+#Auteur         : Alice Petitpierre
+#Python Version : 3.13
+#-----------------------------------------------
 
-from fonctions import selectionner_mot, choisir_fichier, jouer_pendu
+print ('- JEU DU PENDU -')
 
-reponse = input ('Voulez vous jouer avec votre propre ficher de mot (oui/non) :')
-fichier = choisir_fichier (reponse)
-mot = selectionner_mot (fichier)
-resultat = jouer_pendu (mot)
+from module_fonctions import choisir_fichier, jouer_pendu
+
+print ()
+
+while True :
+    reponse=""
+    while reponse not in ["oui","non"]:
+        reponse = input ('Veux-tu jouer avec ton propre fichier de mot ? (oui/non) :').strip().lower()
+        if reponse not in ["oui","non"]:
+            print('Réponse invalide')
+
+    fichier = choisir_fichier (reponse)
+    resultat = jouer_pendu (fichier)
+    print (f'La réponse etait {resultat}')
+
+    nouvelle_partie = input ('Veux-tu jouer de nouveau ? (oui/non) :')
+    if nouvelle_partie == 'oui':
+        continue
+    else :
+        break
--- module_fonctions.py
+++ module_fonctions.py
@@ -10,24 +10,35 @@ def choisir_fichier (reponse) :
 def selectionner_mot (fichier_txt):
     import random
     with open(fichier_txt, "r") as fichier:
-        liste_mots = [ligne.strip() for ligne in fichier.readlines()]
-    return random.choice(liste_mots)
+        liste_mots = [ligne.strip().lower() for ligne in fichier.readlines()]
+        mot = random.choice(liste_mots)
+        mot_normalise = enlever_caracteres_speciaux(mot)
+    return mot_normalise
 
 def enlever_caracteres_speciaux(mot):
     import unicodedata
     normalized_word = unicodedata.normalize('NFKD',mot)
     return ''.join([char for char in normalized_word if not unicodedata.combining(char)])
 
-def jouer_pendu (mot) :
-    mot_normalise = enlever_caracteres_speciaux(mot)
+def trouver_indice (mot) :
+    import string
+    alphabet = string.ascii_lowercase
+    indice = [lettre for lettre in alphabet if lettre not in mot]
+    return indice
+
+
+def jouer_pendu (fichier) :
+
+    mot = selectionner_mot(fichier)
+
     mot_cache = '_'*len(mot)
-    print(mot_cache)
+    print(f'Mot à deviner : {mot_cache}')
 
     chance = 6
     lettre_teste = []
 
     while chance > 0 :
-        lettre = input (f'Il te reste {chance} chance(s), essayer la lettre :').lower()
+        lettre = input (f'Essayer la lettre (tu as {chance} chance(s)) :').lower()
         lettre = enlever_caracteres_speciaux(lettre)
 
         #Verifier que la lettre n'a pas deja été joué
@@ -37,14 +48,15 @@ def jouer_pendu (mot) :
         else :
             lettre_teste += [lettre]
 
-
+        #Ajouter la lettre lorsqu'elle est dans le mot
         trouve = False
-        for i in range(len(mot_normalise)):
-            if lettre == mot_normalise[i]:
+        for i in range(len(mot)):
+            if lettre == mot[i]:
                 mot_cache = mot_cache[:i]+lettre+mot_cache[i+1:]
                 trouve = True
 
-        if mot_cache == mot_normalise :
+        #Sortie de la boucle si le mot est trouvé en entier avant la fin des 6 chances
+        if mot_cache == mot :
             break
         elif trouve :
             print(mot_cache)
@@ -52,10 +64,14 @@ def jouer_pendu (mot) :
             chance -= 1
             print(mot_cache)
             print ("-> La lettre n'est pas dans le mot")
+            #Bonus donner un indice pour la dernière chance
+            if chance == 1 :
+                lettre_indice = trouver_indice(mot)
+                print (f"Indice : {lettre_indice} n'est pas dans le mot")
 
-    if mot_cache == mot_normalise :
-        print (f'Gagné tu as trouvé le mot : {mot_normalise}')
+    if mot_cache == mot :
+        print ('Gagné, tu as trouvé le mot !!')
     if chance == 0 :
         print ('Perdu, tu as epuisé tes chances...')
 
-    return mot_normalise
+    return mot
