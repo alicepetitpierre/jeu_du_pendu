878862fdb6646b103e4ba2171ba7b2dc38787217
alicepetitpierre <alice.petitpierre.1@ens.etsmtl.ca>
2025-05-21 10:01:33 -0400

    Début README

--- jeu_du_pendu.py
+++ jeu_du_pendu.py
@@ -2,22 +2,45 @@ import random
 
 #Definir fonction lire fichier et renvoyer liste des lignes du fichier
 def selectionner_mot (fichier_txt):
-    #ouvrir le fichier en mode lecture
-    fichier = open(fichier_txt, "r")
-    #creer une liste des lignes du fichier
-    liste_mots = fichier.readlines()
-    print(liste_mots)
-    #nettoyer la liste
-    for i in range(len(liste_mots)):
-        liste_mots[i]=liste_mots[i].strip()
-    print(liste_mots)
-    #sortir un mot de facon aléatoire
+    with open(fichier_txt, "r") as fichier:
+        liste_mots = [ligne.strip() for ligne in fichier.readlines()]
     return random.choice(liste_mots)
 
-def tester_lettre (lettre,mot):
-    for i in range (len(mot)):
-        if mot[i]==lettre:
-            return
+def enlever_caracteres_speciaux(mot):
+    import unicodedata
+    normalized_word = unicodedata.normalize('NFKD',mot)
+    return ''.join([char for char in normalized_word if not unicodedata.combining(char)])
 
 mot = selectionner_mot ("mots_pendu.txt")
 print(mot)
+mot_cache = '_'*len(mot)
+print(mot_cache)
+mot_normalise = enlever_caracteres_speciaux(mot)
+
+chance = 6
+while chance > 0 :
+    lettre = input (f'Il te reste {chance} chance(s), essayer la lettre :').lower()
+    lettre = enlever_caracteres_speciaux(lettre)
+    #tester si la lettre est dans le mot
+    trouve = False
+    for i in range(len(mot_normalise)):
+        if lettre == mot_normalise[i]:
+            mot_cache = mot_cache[:i]+lettre+mot_cache[i+1:]
+            trouve = True
+
+    if mot_cache == mot_normalise :
+        break
+    elif trouve :
+        print(mot_cache)
+        print ('-> Bonne pioche')
+    else :
+        chance -= 1
+        print(mot_cache)
+        print ("-> La lettre n'est pas dans le mot")
+
+
+if mot_cache == mot_normalise :
+    print (f'Gagné tu as trouvé le mot : {mot_normalise}')
+if chance == 0 :
+    print ('Perdu, tu as epuisé tes chances...')
+
